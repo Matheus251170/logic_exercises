08402beda6f9b66da5babb0fcfadc0c93f428613
Matheus <matheus251170@outlook.com>
2021-07-31 13:56:18 -0300

    tabuada com do while

---
+++ Tabuada_Do_While/Tabuada_Do_While/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, resp;
+            
+            Console.Title = "Tabuada Do_While";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.Write("Digite um número: ");
+            n = int.Parse(Console.ReadLine());
+
+            x = 1;
+
+            do
+            {
+                resp = n * x;
+
+                Console.WriteLine("{0} x {1} = {2}", n, x, resp);
+                x++;
+
+            } while (x <= 10);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tabuada_Do_While/Tabuada_Do_While/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Tabuada_Do_While")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tabuada_Do_While")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a94ab290-544d-4129-a7f5-ab50cfc31574")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
