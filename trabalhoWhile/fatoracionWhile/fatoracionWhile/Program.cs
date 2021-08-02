9024a3ad2484e0f65050eec27660a01fd469f026
Matheus <matheus251170@outlook.com>
2021-08-02 16:11:07 -0300

    exercicio de fatoração

---
+++ trabalhoWhile/fatoracionWhile/fatoracionWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fatoracionWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, fatorial;
+            fatorial = 1;
+
+            Console.Title = "Fatoração While";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.Write("Escolha um número para fatorar: ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x >= 1)
+            {           
+                fatorial = fatorial *= x;
+                x--;
+            }
+            Console.Write("O fatorial é " + fatorial);
+            Console.ReadKey();
+        }
+    }
+}
