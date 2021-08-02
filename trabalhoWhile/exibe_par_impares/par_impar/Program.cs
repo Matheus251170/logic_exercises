2ba7c64971c2f6f2833a4e3a7dff47a8b8f761b4
Matheus <matheus251170@outlook.com>
2021-08-02 16:09:12 -0300

    exercicio que exibe número impares e pares

---
+++ trabalhoWhile/exibe_par_impares/par_impar/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dificil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, z =  0, y = 0, lim, x, par = 0, imp = 0;
+            char resp, inicio;
+
+        inicio:
+
+            Console.Title = "Par e Impar While";
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            Console.Write("Digite a quantidade de números que você deseja: ");
+            lim = int.Parse(Console.ReadLine());
+            x = 1;
+
+            while (x <= lim)
+            {
+
+                Console.Write("Digite um número: ");
+                n = int.Parse(Console.ReadLine());
+                x++;
+
+               
+                if (n < 0)
+                {
+                    z++;
+                }
+                else 
+                {
+                    y++;
+                }
+                if (n % 2 != 0)
+                {
+                    imp++;
+                }
+                else
+                {
+                    par++;
+                }
+                
+            }
+
+            Console.WriteLine("\nNúmeros impares: {0}", imp++);
+            Console.WriteLine("\nNúmeros pares: {0}", par++);
+
+            Console.Write("Deseja encerrar o programa? Se sim digite 's', se não digite n: ");
+            resp = char.Parse(Console.ReadLine());
+            if ((resp == 'n' || (resp == 'N')))
+            {
+                goto inicio;
+            }
+            else
+            {
+                Console.WriteLine("O Programa será fechado!");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
