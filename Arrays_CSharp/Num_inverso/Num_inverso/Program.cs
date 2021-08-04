6cfbc0bb0d9b879c18e7054229db0dea210c4c20
Matheus <matheus251170@outlook.com>
2021-08-04 14:17:09 -0300

    exercicios de array

---
+++ Arrays_CSharp/Num_inverso/Num_inverso/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Num_inverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int num;
+            int[] numeros = new int[10];
+
+            Console.Write("Insira o primeiro número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            for (i = 0; i < numeros.Length; i++)
+                numeros[i] = (i + 1) + num;
+
+            for (i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("{0}", numeros[i].ToString("#0"));
+            }
+
+            Console.WriteLine("\n\nNúmeros em ordem inversa:\n");
+            i = i - 1;
+
+            while (i>=0)
+            {
+                Console.WriteLine("{0}", numeros[i].ToString("#0"));
+                i--;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arrays_CSharp/SalarioMinimo/SalarioMinimo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioMinimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double sal_minimo;
+            double[] salarios = new double[10];
+
+            Console.Write("Insira seu salário: ");
+            sal_minimo = Convert.ToDouble(Console.ReadLine());
+
+                for (i = 0; i < salarios.Length; i++)
+                salarios[i] = (i + 1) * sal_minimo;
+            
+            for (i = 0; i < salarios.Length; i++)
+                Console.WriteLine("\n{0} Salário(s) Minimo(s) = {1}", (i + 1), salarios[i].ToString("#00.00"));
+
+            Console.ReadKey();
+        }
+    }
+}
