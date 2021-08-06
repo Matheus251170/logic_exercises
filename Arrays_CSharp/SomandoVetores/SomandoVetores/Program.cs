14b0414fdd2ade2e1991a54adf8830a7a8808cec
Matheus <matheus251170@outlook.com>
2021-08-05 22:16:00 -0300

    exercicio de soma de vetores

---
+++ Arrays_CSharp/SomandoVetores/SomandoVetores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomandoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = 3;
+            int[] A = new int [tamanho];
+            int[] B = new int[tamanho];
+            int[] R = new int[tamanho];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine("Digite os elementos de A [{0}]: ", i);
+                A[i] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Digite os elementos de B [{0}]: ", i);
+                B[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < R.Length; i++)
+            {
+                R[i] = A[i] + B[i];
+            }
+
+            Console.WriteLine("Valores armazenados no vetor C são: ");
+            for(int i = 0; i < R.Length; i++)
+            {
+                Console.WriteLine("R[{0}]: {1} ", i, R[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
