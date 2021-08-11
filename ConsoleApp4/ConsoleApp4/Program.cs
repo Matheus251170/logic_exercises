8998c3d553404c3867b2dd91d709a384f2edbedb
Matheus <matheus251170@outlook.com>
2021-08-11 18:37:46 -0300

    adiciona exercicio de C#

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Diferença";
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            int x, n, valor;
+           
+            Console.Write("Digite o primeiro número: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            n = int.Parse(Console.ReadLine());
+
+
+             valor = Diferenca(x, n);
+
+            Console.WriteLine("\n\nA diferença entre os números é {0}!", valor);
+            Console.ReadKey();
+        }
+
+        static int Diferenca(int x, int n)
+        {
+            int dif = x - n;
+            return dif;
+           
+        }
+    }
+}
